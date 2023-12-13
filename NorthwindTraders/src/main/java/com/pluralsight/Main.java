9686dc6087305ba800ecd89389f2c673472d6e14
Sing Tuttanon <singtuttanon@gmail.com>
2023-12-13 12:29:42 -0500

    DataSource Implementation: Exercise 1 Complete

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,21 +1,25 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
+
+    private static final String URL = "jdbc:mysql://localhost:3306/northwind";
+    private static final String USER = "root";
+    private static final String PASS = "~CONFIDENTIAL~";
 
     public static void main(String[] args) {
-        // Database (/Query) Information.
-        Connection connection = null;
-        String url = "jdbc:mysql://localhost:3306/northwind";
-        String username = "root";
-        String password = "~CONFIDENTIAL~";
+        // Database (/Query) Information |  Data Source Implementation.
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USER);
+        dataSource.setPassword(PASS);
 
         //  Main Menu Flow
-        try {
-            connection = DriverManager.getConnection(url, username, password);
+        try (Scanner scan = new Scanner(System.in)) {
             int option;
 
             do {
@@ -33,15 +37,15 @@ public class Main {
                 switch (option) {
                     case 1:
                         //  "Display All Products"
-                        option1(connection);
+                        option1(dataSource);
                         break;
                     case 2:
                         //  "Display All Customers"
-                        option2(connection);
+                        option2(dataSource);
                         break;
                     case 3:
                         //  "Display All Categories"
-                        option3(connection);
+                        option3(dataSource, scan);
                         break;
                     case 0:
                         System.out.println("Exiting now... Have A Good Day!");
@@ -50,30 +54,20 @@ public class Main {
                         System.out.println("Invalid Option. Redirecting...\n");
                         break;
                 }
+
             } while (option != 0);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     //  Display all Product Information.
-    private static void option1(Connection connection) {
+    private static void option1(BasicDataSource dataSource) {
+    //   SQL Query
         String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+    //  Header Display
             System.out.println("\t\t\t\t\t-----Product Inventory-----\n");
             System.out.printf("%-10s %-40s %-10s %-10s%n", "ID", "Name", "Price", "Stock");
             System.out.println("--------------------------------------------------------------------------");
@@ -86,7 +80,6 @@ public class Main {
 
                 System.out.printf("%-10d %-40s %-10.2f %-10d%n", id, name, price, stock);
             }
-            resultSet.close();
             System.out.println("\n\t\t\t\t-----Redirecting to Main Menu-----\n");
 
         } catch (SQLException e) {
@@ -95,12 +88,14 @@ public class Main {
     }
 
     // Display all Customer Information.
-    private static void option2(Connection connection) {
+    private static void option2(BasicDataSource dataSource) {
+    //   SQL Query
         String query = "SELECT ContactName, CompanyName, City, Country, Phone FROM Customers ORDER BY Country";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+    //  Header Display
             System.out.println("\t\t\t\t\t\t\t\t\t\t\t------Customer Inventory-----\n");
             System.out.printf("%-25s %-37s %-20s %-20s %-15s%n", "Contact Name", "Company Name", "City", "Country", "Phone");
             System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
@@ -114,7 +109,6 @@ public class Main {
 
                 System.out.printf("%-25s %-37s %-20s %-15s %-15s%n", nameContact, nameCompany, city, country, phone);
             }
-            resultSet.close();
             System.out.println("\n\t\t\t\t\t\t\t\t\t\t------Redirecting to Main Menu-----\n");
 
         } catch (SQLException e) {
@@ -123,12 +117,14 @@ public class Main {
     }
 
     //  Display All Categories.
-    private static void option3(Connection connection) {
+    private static void option3(BasicDataSource dataSource, Scanner scan) {
+    //   SQL Query
         String query = "SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+    //  Header Display
             System.out.println("\t\t------Category Inventory-----\n");
             System.out.printf("%-20s %-40s%n", "Category ID", "Category Name");
             System.out.println("-------------------------------------------------------");
@@ -139,13 +135,13 @@ public class Main {
 
                 System.out.printf("%-20d %-40s%n", id, name);
             }
-            resultSet.close();
 
+    //  Prompt For Category ID --> For Product Information Under Specified Category <--> '0' To Go Back to Main Menu.
             System.out.println("\nChoose A Category (ID) To View Products Information | Enter '0' To Go Back To The Main Menu: \n");
             int inputID = scan.nextInt();
 
             if (inputID != 0) {
-                categoryProducts(connection, inputID);
+                categoryProducts(dataSource, inputID);
             } else {
                 System.out.println("\n\t\t\t------Redirecting to Main Menu-----\n");
             }
@@ -156,24 +152,28 @@ public class Main {
     }
 
     //  Display All Products From Specified Category.
-    private static void categoryProducts(Connection connection, int categoryID) {
+    private static void categoryProducts(BasicDataSource dataSource, int categoryID) {
+    //   SQL Queries
         String queryProducts = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products WHERE CategoryID = ?";
         String queryCategoryName = "SELECT CategoryName FROM Categories WHERE CategoryID = ?";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(queryCategoryName)) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(queryCategoryName)) {
+
             preparedStatement.setInt(1, categoryID);
             ResultSet resultSet = preparedStatement.executeQuery();
             String categoryName = "";
 
             if (resultSet.next()) {
-            categoryName = resultSet.getString("CategoryName");
+                categoryName = resultSet.getString("CategoryName");
             }
-
-            System.out.println("\t\t\t\t------Product Inventory In " + categoryName + "-----\n");
+    //  Header Display
+            System.out.println("\t\t\t\t------Inventory For " + categoryName + "-----\n");
             System.out.printf("%-10s %-40s %-10s %-10s%n", "ID", "Name", "Price", "Stock");
             System.out.println("-----------------------------------------------------------------------------");
 
             try (PreparedStatement productsStatement = connection.prepareStatement(queryProducts)) {
+
                 productsStatement.setInt(1, categoryID);
                 ResultSet productSet = productsStatement.executeQuery();
 
@@ -185,7 +185,6 @@ public class Main {
 
                     System.out.printf("%-10d %-40s %-10.2f %-10d%n", id, name, price, stock);
                 }
-                productSet.close();
 
             } catch (SQLException e) {
                 e.printStackTrace();
